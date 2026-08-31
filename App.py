f50444d5a91d30780c11a2b8eb61503b998a5266
mohanram-21 <mohanramv21@gmail.com>
2026-08-31 14:58:12 +0530

    Add login validation (#4)

--- App.py
+++ App.py
@@ -6,3 +6,11 @@ print ("Started development of module 2 / dashboard page ")
 
 print("Main team update!")
 print("new login updated!")
+
+username = "admin"
+password = ""
+
+if password == "":
+    print("Password is required")
+else:
+    print("Login successful")
